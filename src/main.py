267da621eac4e9c6f109a4010582258461f3c966
Prince Maphupha <princemaphupha@Princes-Air.home>
2024-11-30 13:07:42 +0200

    Chore: adjusted screen width & height

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 
 pygame.init()
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 700
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame Boilerplate")
 
