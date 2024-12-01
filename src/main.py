dd00900f8afd421d45110e3fa45d164f191b7736
Prince Maphupha <princemaphupha@Princes-Air.home>
2024-12-01 15:47:08 +0200

    Chore: Added a start menu

--- src/main.py
+++ src/main.py
@@ -28,10 +28,32 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 700
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("HoleInOne")
 
+# Starter Screen
+
+logoWidth = 300
+logoHeight = logoWidth / 2
+logoX = (SCREEN_WIDTH - logoWidth) / 2
+logoY = 100
+
+buttonImageWidth = 200
+buttonImageHeight = buttonImageWidth / 2
+buttonImageX = (SCREEN_WIDTH - buttonImageWidth) / 2
+def buttonImageY(num):
+    return 200 + (100 * num)
+
+logos = []
+logos.append(loadImage("logo1.png", logoWidth, logoHeight))
+logos.append(loadImage("logo2.png", logoWidth, logoHeight))
+play_img = loadImage("play.png", buttonImageWidth, buttonImageHeight)
+exit_img = loadImage("exit.png", buttonImageWidth, buttonImageHeight)
+
+starter_img = loadImage("starter_back.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
 # Classes
 
 friction = 0.08
-world = World(friction, 3, 108, True)
+world = World(friction, random.randint(0, 8), random.randint(0, 359), True)
 
 
 ball = GolfBall(100, SCREEN_HEIGHT / 2, 20, 20)
@@ -75,9 +97,6 @@ else:
     print("No joystick detected!")
     joystick = None
 
-
-parCount = 3
-
 font = pygame.font.Font('freesansbold.ttf', 15)
 text = font.render(str(world.windSpeed) + " / " + str(world.windDirection), True, BLACK)
 textRect = text.get_rect()
@@ -93,7 +112,11 @@ textRect3.center = (200, 50)
 textRect4 = text.get_rect()
 textRect4.center = (300, 50)
 
+textRect5 = text.get_rect()
+textRect5.center = (400, 50)
+
 running = True
+started = False
 play = False
 
 count = 0
@@ -101,9 +124,11 @@ count = 0
 
 holeCount = 0
 hits = 0
+total_hits = 0
 holeLevel = None
 obstacles = []
 sandPatches = []
+noPlayCount = 0
 
 def gameInit():
     global obstacles
@@ -112,6 +137,7 @@ def gameInit():
     global holeCount
     global hits
     global ball
+    global holeLevel
 
     ball.x = 100
     ball.y = SCREEN_HEIGHT / 2
@@ -133,10 +159,10 @@ def gameInit():
     parSum = 0
 
     for x in range(obstacleCount):
-        obstacles.append(Obstacle(random.randint(100, SCREEN_WIDTH - 100), random.randint(50, SCREEN_HEIGHT - 50), 50))
+        obstacles.append(Obstacle(random.randint(100, SCREEN_WIDTH - 200), random.randint(50, SCREEN_HEIGHT - 50), 50))
 
     for y in range(sandPatchCount):
-        sandPatches.append(SandPatch(random.randint(100, SCREEN_WIDTH - 100), random.randint(50, SCREEN_HEIGHT - 50), random.randint(50, 100), random.randint(50, 100)))
+        sandPatches.append(SandPatch(random.randint(100, SCREEN_WIDTH - 200), random.randint(50, SCREEN_HEIGHT - 50), random.randint(50, 100), random.randint(50, 100)))
 
     holeMoveGuess = random.randint(0, 8000) % 2
     if holeMoveGuess == 0:
@@ -156,124 +182,162 @@ def gameInit():
     par = round(parSum / 4)
 
     holeLevel = HoleLevel(sandPatchCount, obstacleCount, holeMove, par, holeMoveRange, holeMoveVeloc)
+
+    hole.x = SCREEN_WIDTH - 100
+    hole.y = random.randint(50, SCREEN_HEIGHT - 50)
     
 
 
 while running:
-    # Event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    if started:
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        # Joystick button press handling
-        if event.type == pygame.JOYBUTTONDOWN:
-            if event.button == 0:  # A button is usually button 0 on Xbox controllers
-                if ball.getVelocity() == 0 and play:
-                    ball.vX = ball.hitVelocity * (axis_x / math.hypot(axis_x, axis_y)) * (math.hypot(axis_x, axis_y) / 1)
-                    ball.vY = ball.hitVelocity * (axis_y / math.hypot(axis_x, axis_y)) * (math.hypot(axis_x, axis_y) / 1)
-                    ball.xRatio = (axis_x / math.hypot(axis_x, axis_y))
-                    ball.yRatio = (axis_y / math.hypot(axis_x, axis_y))
-                    hits += 1
+            # Joystick button press handling
+            if event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 0:  # A button is usually button 0 on Xbox controllers
+                    if ball.getVelocity() == 0 and play:
+                        ball.vX = ball.hitVelocity * (axis_x / math.hypot(axis_x, axis_y)) * (math.hypot(axis_x, axis_y) / 1)
+                        ball.vY = ball.hitVelocity * (axis_y / math.hypot(axis_x, axis_y)) * (math.hypot(axis_x, axis_y) / 1)
+                        ball.xRatio = (axis_x / math.hypot(axis_x, axis_y))
+                        ball.yRatio = (axis_y / math.hypot(axis_x, axis_y))
+                        hits += 1
+                        total_hits += 1
 
-    if not play:
-        gameInit()
+        if not play:
+            gameInit()
 
-    if play:
-        ball.x += ball.vX
-        ball.y += ball.vY
-        if ball.getVelocity() > 0:
-            if ball.x > 0 and ball.x < SCREEN_WIDTH - ball.w:
-                ball.x += (world.windSpeed / 5)  * math.cos(world.windDirection)
-            if ball.y > 0 and ball.y < SCREEN_HEIGHT - ball.h:
-                ball.y += (world.windSpeed / 5) * math.sin(world.windDirection)
+        if play:
+            ball.x += ball.vX
+            ball.y += ball.vY
+            if ball.getVelocity() > 0:
+                if ball.x > 0 and ball.x < SCREEN_WIDTH - ball.w:
+                    ball.x += (world.windSpeed / 5)  * math.cos(world.windDirection)
+                if ball.y > 0 and ball.y < SCREEN_HEIGHT - ball.h:
+                    ball.y += (world.windSpeed / 5) * math.sin(world.windDirection)
+                
+            
+        # Applying Friction
+        veloc = ball.getVelocity()
+        if veloc >= world.friction:
+            veloc -= world.friction
+            ball.vX = veloc * ball.xRatio
+            ball.vY = veloc * ball.yRatio
+        else:
+            ball.vX = 0
+            ball.vY = 0
+
+        if ball.x < 0:
+            ball.xRatio = abs(ball.xRatio)
+
+        if ball.x > (SCREEN_WIDTH - ball.w):
+            ball.xRatio = -(abs(ball.xRatio))
+
+        if ball.y < 0:
+            ball.yRatio = abs(ball.yRatio)
+
+        if ball.y > (SCREEN_HEIGHT - ball.h):
+            ball.yRatio = -(abs(ball.yRatio))
+
+        # Joystick axis movement (e.g., move ball with left joystick)
+        if joystick:
+            axis_x = joystick.get_axis(0)  
+            axis_y = joystick.get_axis(1)  
+
+        # Drawing
+        screen.blit(background, (0, 0))
+        if ball.getVelocity() > 1:
+            num = int(count / (ball.hitVelocity - ball.getVelocity()))  % 2
+            ball_image = ball_images[num]
+        
+        
+        if cartesian(ball.x + (ball.w  / 2), ball.y + (ball.h / 2), hole.x + (hole.w / 2), hole.y + (hole.h / 2)) < ((hole.w / 2) + (ball.w / 2)):
+            play = False
+        
+
+        for y in sandPatches:
+            pygame.draw.rect(screen, y.color, (y.x, y.y, y.w, y.h))
+
+            hitSand = collision(ball.x + (ball.w / 2), ball.y + (ball.h / 2), ball.w / 2, y.x, y.y, y.w, y.h)
+            if hitSand[0]:
+                world.friction = world.friction * 2
+            else:
+                if world.rain:
+                    world.friction = friction * 0.75
+                else:
+                    world.friction = friction
+
+        for x in obstacles:
+            screen.blit(obstacle_img, (x.x, x.y))
+            hitObs = collision(ball.x + (ball.w / 2), ball.y + (ball.h / 2), ball.w / 2, x.x, x.y, x.w, x.h)
+            if hitObs[0]:
+                s = hitObs[1]
+                if s == 1:
+                    ball.xRatio = -(abs(ball.xRatio))
+                elif s == 2:
+                    ball.yRatio = -(abs(ball.yRatio))
+                elif s == 3:
+                    ball.xRatio = (abs(ball.xRatio))
+                elif s == 4:
+                    ball.yRatio = abs(ball.yRatio)
+
+        if play:
+            screen.blit(ball_image, (ball.x, ball.y))
             
         
-    # Applying Friction
-    veloc = ball.getVelocity()
-    if veloc >= world.friction:
-        veloc -= world.friction
-        ball.vX = veloc * ball.xRatio
-        ball.vY = veloc * ball.yRatio
-    else:
-        ball.vX = 0
-        ball.vY = 0
-
-    if ball.x < 0:
-        ball.xRatio = abs(ball.xRatio)
-
-    if ball.x > (SCREEN_WIDTH - ball.w):
-        ball.xRatio = -(abs(ball.xRatio))
-
-    if ball.y < 0:
-        ball.yRatio = abs(ball.yRatio)
-
-    if ball.y > (SCREEN_HEIGHT - ball.h):
-        ball.yRatio = -(abs(ball.yRatio))
-
-    # Joystick axis movement (e.g., move ball with left joystick)
-    if joystick:
-        axis_x = joystick.get_axis(0)  
-        axis_y = joystick.get_axis(1)  
-
-    # Drawing
-    screen.blit(background, (0, 0))
-    if ball.getVelocity() > 1:
-        num = int(count / (ball.hitVelocity - ball.getVelocity()))  % 2
-        ball_image = ball_images[num]
-    
-    
-    if cartesian(ball.x + (ball.w  / 2), ball.y + (ball.h / 2), hole.x + (hole.w / 2), hole.y + (hole.h / 2)) < ((hole.w / 2) + (ball.w / 2)):
-        play = False
-    
-
-    for y in sandPatches:
-        pygame.draw.rect(screen, y.color, (y.x, y.y, y.w, y.h))
-
-        hitSand = collision(ball.x + (ball.w / 2), ball.y + (ball.h / 2), ball.w / 2, y.x, y.y, y.w, y.h)
-        if hitSand[0]:
-            world.friction = world.friction * 2
-        else:
-            if world.rain:
-                world.friction = friction * 0.75
-            else:
-                world.friction = friction
-
-    for x in obstacles:
-        screen.blit(obstacle_img, (x.x, x.y))
-        hitObs = collision(ball.x + (ball.w / 2), ball.y + (ball.h / 2), ball.w / 2, x.x, x.y, x.w, x.h)
-        if hitObs[0]:
-            s = hitObs[1]
-            if s == 1:
-                ball.xRatio = -(abs(ball.xRatio))
-            elif s == 2:
-                ball.yRatio = -(abs(ball.yRatio))
-            elif s == 3:
-                ball.xRatio = (abs(ball.xRatio))
-            elif s == 4:
-                ball.yRatio = abs(ball.yRatio)
-
-    if play:
-        screen.blit(ball_image, (ball.x, ball.y))
+        screen.blit(hole_img, (hole.x, hole.y))
+        screen.blit(windArrow_img, (800, 50))
+        text2 = font.render("Hole: " + str(holeCount), True, BLACK)
+        text3 = font.render("Par: " + str(holeLevel.par), True, BLACK)
+        text4 = font.render("Hits: " + str(hits), True, BLACK)
+        text5 = font.render("Total Hits: " + str(total_hits), True, BLACK)
+        screen.blit(text, textRect)
+        screen.blit(text2, textRect2)
+        screen.blit(text3, textRect3)
+        screen.blit(text4, textRect4)
+        screen.blit(text5, textRect5)
         
-    
-    screen.blit(hole_img, (hole.x, hole.y))
-    screen.blit(windArrow_img, (800, 50))
-    text2 = font.render("Hole: " + str(holeCount), True, BLACK)
-    text3 = font.render("Par: " + str(parCount), True, BLACK)
-    text4 = font.render("Hits: " + str(hits), True, BLACK)
-    screen.blit(text, textRect)
-    screen.blit(text2, textRect2)
-    screen.blit(text3, textRect3)
-    screen.blit(text4, textRect4)
-    
 
-    # Update display
-    pygame.display.flip()
+        # Update display
+        pygame.display.flip()
 
-    count += 1
+        count += 1
 
-    # Control the frame rate
-    clock.tick(FPS)
+        # Control the frame rate
+        clock.tick(FPS)
+
+    else:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Set the x, y postions of the mouse click
+                x, y = event.pos
+                if x > buttonImageX and x < buttonImageX + buttonImageWidth:
+                    if y > buttonImageY(1) and y < buttonImageY(1) + buttonImageHeight:
+                        # Play
+                        started = True
+                    elif y > buttonImageY(2) and y < buttonImageY(2) + buttonImageHeight:
+                        running = False
+
+        screen.blit(starter_img, (0, 0))
+        if int(count / 60) % 2 == 0:
+            screen.blit(logos[0], (logoX, logoY))
+        else:
+            screen.blit(logos[1], (logoX, logoY))
+        screen.blit(play_img, (buttonImageX, buttonImageY(1)))
+        screen.blit(exit_img, (buttonImageX, buttonImageY(2)))
+
+        
+
+        pygame.display.flip()
+
+        count += 1
+
+        clock.tick(FPS)
 
 pygame.quit()
 sys.exit()
